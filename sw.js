9de554d0b70fc655040d3506581b920dfda92cfd
CPACT <67503583+CPACT@users.noreply.github.com>
2026-08-05 15:32:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="acp-shell-pwa-v51";
+const CACHE="acp-shell-pwa-v54";
 const CORE=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
 
 self.addEventListener("install",event=>{
